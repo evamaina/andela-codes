35999a9e0f1b6cc8f83b6607bbae1e9d586c1b86
Eva <evajohnson714@gmail.com>
2017-10-15 18:45:40 +0300

    modify the changes

--- tripcost.py
+++ tripcost.py
@@ -19,5 +19,7 @@ def rental_car_cost(days):
     cost -= 20
   return cost
 
-def trip_cost(city, days):
-  return rental_car_cost(days) + hotel_cost(days) + plane_ride_cost(city)
+def trip_cost(city,days,spending_money):
+  return rental_car_cost(days) + hotel_cost(days) + plane_ride_cost(city) + spending_money
+
+print trip_cost("Los Angeles", 5, 600)
